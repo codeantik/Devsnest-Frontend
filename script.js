c58e6682b12d3374cc33c3d8c38d91eb14c3a358
Ankit Singh <ankitsinankitsin888000@gmail.com>
2021-06-16 23:51:35 +0530

    Commented console.log

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // "use strict"
 const divs = document.querySelectorAll('.square')
-console.log(divs);
+// console.log(divs);
 
 const leave = [130, 131, 149, 150, 151, 152, 168, 169, 170, 171, 172, 173, 187, 188, 190, 191, 193, 194, 207, 208, 209, 210, 211, 212, 213, 214, 229, 232, 248, 250, 251, 253, 267, 269, 272, 274];
 
