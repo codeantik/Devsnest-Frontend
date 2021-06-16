65515cc73e8e2de4abe0053a45796c65de25010a
Ankit Singh <ankitsinankitsin888000@gmail.com>
2021-06-16 15:02:38 +0530

    Removed comments

--- script.js
+++ script.js
@@ -1,90 +1,90 @@
 // Task 1
 
-// const listProperties = (student) => {
-//     if(typeof(student) !== 'object') return []
-//     if(Object.keys(student)) return Object.keys(student)
-//     return []
-// }
+const listProperties = (student) => {
+    if(typeof(student) !== 'object') return []
+    if(Object.keys(student)) return Object.keys(student)
+    return []
+}
 
-// const student = {
-//     name : "David Rayy",
-//     class : "VI",
-//     rollno : 12 
-// }
-// // const student = "this is a string"
+const student = {
+    name : "David Rayy",
+    class : "VI",
+    rollno : 12 
+}
+// const student = "this is a string"
 
-// console.log(listProperties(student))
+console.log(listProperties(student))
 
 // Task 2
 
-// const deleteProperty = (student) => {
-//     delete student.rollno
-//     console.log(student)
-// }
+const deleteProperty = (student) => {
+    delete student.rollno
+    console.log(student)
+}
 
-// const student = {
-//     name: "Ankit Singh",
-//     class: "UKG",
-//     rollno: 5
-// }
+const student = {
+    name: "Ankit Singh",
+    class: "UKG",
+    rollno: 5
+}
 
-// deleteProperty(student)
+deleteProperty(student)
 
 // Task 3
 
-// const getLength = (student) => {
-//     return Object.keys(student).length
-// }
+const getLength = (student) => {
+    return Object.keys(student).length
+}
 
-// const student = {
-//     name: "Ankit Singh",
-//     class: "UKG",
-//     rollno: 5
-// }
+const student = {
+    name: "Ankit Singh",
+    class: "UKG",
+    rollno: 5
+}
 
-// console.log(getLength(student))
+console.log(getLength(student))
 
 // Task 4
 
-// const readingStatus = (library) => {
-//     library.map(({ title, author, readingStatus }) => {
-//         console.log((readingStatus ? "Read " : "Not read ") + " " + title + " by " + author)
-//     })
-// }
+const readingStatus = (library) => {
+    library.map(({ title, author, readingStatus }) => {
+        console.log((readingStatus ? "Read " : "Not read ") + " " + title + " by " + author)
+    })
+}
 
-// const library = [ 
-//     {
-//         title: 'Bill Gates',
-//         author: 'The Road Ahead',
-//         readingStatus: true
-//     },
-//     {
-//         title: 'Steve Jobs',
-//         author: 'Walter Isaacson',
-//         readingStatus: true
-//     },
-//     {
-//         title: 'Mockingjay: The Final Book of The Hunger Games',
-//         author: 'Suzanne Collins',
-//         readingStatus: false
-//     }];
+const library = [ 
+    {
+        title: 'Bill Gates',
+        author: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        title: 'Steve Jobs',
+        author: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        author: 'Suzanne Collins',
+        readingStatus: false
+    }];
 
-// readingStatus(library)
+readingStatus(library)
 
 // Task 5
 
-// class volume {
-//     constructor(radius, height) {
-//         this.radius = radius
-//         this.height = height
-//     }
+class volume {
+    constructor(radius, height) {
+        this.radius = radius
+        this.height = height
+    }
 
-//     getVolume = () => {
-//         return Math.PI * this.radius * this.radius * this.height
-//     }
-// }
-// const obj = new volume(4, 6)
-// console.log("The volume is = " + obj.getVolume().toFixed(4))
+    getVolume = () => {
+        return Math.PI * this.radius * this.radius * this.height
+    }
+}
+const obj = new volume(4, 6)
+console.log("The volume is = " + obj.getVolume().toFixed(4))
 
 
 // Task 6
