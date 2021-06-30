fa4674d9c04ef3b22d58cfa041d3f5d414a8815e
Ankit <ankit.sin099@gmail.com>
2021-06-30 22:47:56 +0530

    first commit

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-let API_KEY = `iHWNo36RdXk3wiJTyX73LjvdxzSBr3EM`
-// let URL = `https://api.giphy.com/v1/gifs/search?api_key=&limit=&q=`
-let searchBtn = document.querySelector('#btn')
+const btn = document.querySelector('.btn')
+console.log(btn)
 
+// click
+btn.addEventListener('mouseover', () => {
+    btn.innerHTML = "mouseover"
+})
 
-const display = () => {
-    btn.addEventListener('click', e => {
-        e.preventDefault()
-        let val = document.querySelector('#search').value
-        if(val === '') return
-        let query = val.trim()
-        fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=1&q=${query}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                let img = document.querySelector('#gif')
-                img.setAttribute('src', data.data[0].images.downsized.url)
-                document.querySelector('#serach').value = ''
-            })
-            .catch(err => console.log(err))
-    })
-}
+btn.addEventListener('mouseout', () => {
+    btn.innerHTML = "mouseout"
+})
 
-document.addEventListener('DOMContentLoaded', display)
