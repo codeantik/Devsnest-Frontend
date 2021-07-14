5d458036a125f95baca458a816f8db5c13be40a4
Ankit <ankit.sin099@gmail.com>
2021-07-14 16:53:14 +0530

    initial

--- calorie-list/src/App.js
+++ calorie-list/src/App.js
@@ -1,31 +1,79 @@
 import './App.css';
 import List from './comps/List';
-import { data } from './comps/Data';
 import { useState } from 'react';
+import addIcon from './assets/add.svg'
 
 function App() {
-  console.log(data)
-  const [items, setItems] = useState(data)
+  const [items, setItems] = useState([
+    {
+      name: 'Pizza',
+      calorie: 24
+    },
+    {
+      name: 'Momos',
+      calorie: 35
+    }
+  ])
 
-  const deleteItem = (id) => {
-    setItems(items.filter((item, idx) => id !== idx))
+
+  const [name, setName] = useState('')
+  const [calorie, setCalorie] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setItems([...items, 
+      {
+        name, 
+        calorie,
+      }
+    ])
+    setName('')
+    setCalorie('')
   }
 
   return (
     <div className='container'>
       <h1 className='list'>Calorie List</h1>
-      <div className='calorie-list'>
-         {items.length
-            ? items.map((item, idx) => (
-              <List
-                key = { idx }
-                id = { idx }
-                item = { item }
-                deleteItem = { deleteItem }
-              />
-            ))
-            : (<h2 className='empty'>List is empty</h2>)
-          }
+      <div className='todo'>
+        <form action="" onSubmit={ handleSubmit }>
+          <label htmlFor="name"> Title </label>
+          <input
+            type='text'
+            placeholder='Type a Food name'
+            name='name'
+            value={ name }
+            onChange={e => setName(e.target.value)}
+            required
+          />
+          <label htmlFor='calorie'> Calorie </label>
+          <input
+            type='number'
+            placeholder='Give Calorie count'
+            name='calorie'
+            value={ calorie }
+            onChange={e => setCalorie(e.target.value)}
+            required
+          />
+          <button type='submit'>
+            <img src={ addIcon } alt="addIcon" />
+          </button>
+        </form>
+      </div>
+      <div className="calorie-list">
+        {items.length ? (
+          items.map((item, index) => (
+            <List
+              item={ item }
+              key={ index }
+              id={ index }
+              items={ items }
+              setItems={ setItems }
+            />
+          ))
+        ) : (
+          <h2 className='empty'>Add Items</h2>
+        )
+        }
       </div>
     </div>
   );
--- calorie-list/src/comps/List.js
+++ calorie-list/src/comps/List.js
@@ -1,16 +1,78 @@
+import { useState } from 'react'
 import deleteIcon from '../assets/delete.svg'
+import editIcon from '../assets/edit.svg'
+import saveIcon from '../assets/save.svg'
 
-const List = ({ id, item, deleteItem }) => {
-    const { name, cal } = item
+const List = ({ item, id, items, setItems }) => {
+    
+    const [isEditing, setIsEditing] = useState(false)
+    const [editData, setEditData] = useState({
+        name: item.name,
+        calorie: item.calorie
+    })
+
+    const deleteItem = id => setItems(items.filter((item, idx) => idx !== id))
+  
+    const editItem = (e) => {
+        const name = e.target.name
+        const calorie = e.target.value
+        setEditData(prevData => ({
+            ...prevData,
+            [name]: calorie
+        }))
+    }
+
+    const saveItem = (e) => {
+        const updatedItems = items.map((item, idx) => {
+            if(idx === id) {
+                return {
+                    ...item,
+                    name: editData.name,
+                    calorie: editData.calorie
+                }
+            }
+            else {
+                return item
+            }
+        })
+        setItems(updatedItems)
+        setIsEditing(false)
+    }
+
         return (
             <>
                 <div className='cards'>
-                    <div>
-                        <h2>{ name }</h2>
-                        <p>This contains { cal } calories</p>
+                    <div className='inputs'>
+                        {isEditing ? (
+                            <>
+                                <input
+                                    type='text'
+                                    placeholder='Edit Food name'
+                                    name='name'
+                                    value= { editData.name }
+                                    onChange={ editItem }
+                                    required 
+                                />
+                                <input
+                                    type='number'
+                                    placeholder='Edit calorie count'
+                                    name='calorie'
+                                    value= { editData.calorie }
+                                    onChange={ editItem }
+                                    required
+                                />  
+                            </>
+                        ) : (
+                            <>
+                                <h2 className='name'>{ item.name }</h2>
+                                <p className='calorie'>This contains { item.calorie } calories</p>
+                            </>
+                        )}
                     </div>
-                    <div>
-                        <img className='del-btn' src={deleteIcon} alt='img' onClick = { (e) => deleteItem(id) }/>
+                    <div className='icons'>
+                        <img className='edit-btn' src={ editIcon } alt="editIcon" onClick = { e => setIsEditing(!isEditing) }/>
+                        <img className='save-btn' src={ saveIcon } alt="saveIcon" onClick = { e => saveItem() }/>
+                        <img className='del-btn' src={ deleteIcon } alt='deleteIcon' onClick = { e => deleteItem(id) }/>
                     </div>
                 </div>
             </>
