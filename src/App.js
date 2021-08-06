a359f14b6da6a431140b6a667f571ecc30dd26ba
Ankit <ankit.sin099@gmail.com>
2021-08-06 19:04:29 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import ProductListing from './components/ProductListing';
+import ProductDetail from './components/ProductDetail';
+import CartCheckout from './components/CartCheckout';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/' exact component={ProductListing} />
+          <Route path='/product/:productId' exact component={ProductDetail} />
+          <Route path='/cart' exact component={CartCheckout} />
+          <Route path='/payment' exact>Complete the payment process</Route>
+          <Route>404 Not Found!!</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
